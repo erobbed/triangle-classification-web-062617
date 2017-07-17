0d8de7cd95b69568f3c7bea95d4ad336a98f7707
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-07-16 20:00:20 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = [@a, @b, @c].sort
+  end
+
+  def kind
+    if @sides.all?{|s| s > 0} && (@sides[0] + @sides[1] > @sides[2])
+      # binding.pry
+      case @sides.uniq.size
+      when 1
+        :equilateral
+      when 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
+  puts "You have flouted the laws of triangle equality!"
 end
